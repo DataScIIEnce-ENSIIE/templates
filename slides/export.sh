86dbda11178ea2756c7b7ffa7e53f881a36054b9
lcsrodriguez <lucasrodriguez.08@outlook.com>
2022-09-16 22:24:34 +0200

    Adding new params

--- slides/export.sh
+++ slides/export.sh
@@ -10,22 +10,24 @@ EXPORT_TEMPLATE="template_beamer.latex"
 # Export command
 pandoc \
     -s main.md -o $EXPORT_FOLDER/$EXPORT_FILE \
-    -V colorlinks=true \
-    -V linkcolor=blue \
-    -V urlcolor=red \
-    -V toccolor=blue \
     --metadata title="Titre du document" \
     --metadata author="Association DataScIIEnce" \
     --metadata date="Date du document" \
-    --metadata papersize="a4" \
-    --metadata classoption="landscape, twocolumn" \
-    --metadata geometry="top=20mm, bottom=20mm, left=20mm, right=20mm" \
     --metadata colorlinks="true" \
     --metadata linkcolor="red" \
     --metadata urlcolor="blue" \
-    --toc \
-    -V toc-title:"Sommaire" \
-    --template=$EXPORT_FOLDER/$EXPORT_TEMPLATE
+    \
+    --metadata lang="en-US" \
+    --metadata section-titles="false" \
+    --metadata toc="true" \
+    --metadata titlegraphic="img/logo_DS_alt-alt.png" \
+    \
+    -t beamer #-V toc-title:"Sommaire " \
+#--metadata theme="Berlin" \
+#--metadata papersize="a4" \
+#--metadata classoption="landscape, twocolumn" \
+#--metadata geometry="top=20mm, bottom=20mm, left=20mm, right=20mm" \
+#--template=$EXPORT_FOLDER/$EXPORT_TEMPLATE
 
 # Good bye
 echo "Export done"
